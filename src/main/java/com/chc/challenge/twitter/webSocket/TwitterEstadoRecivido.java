9a17368be1506d587c9ab1928b944a8b4235c893
carloshdzcasares <chcandristudio@gmail.com>
2021-04-05 05:57:35 +0200

    Pruebas de despliegue

--- src/main/java/com/chc/challenge/twitter/webSocket/TwitterEstadoRecivido.java
+++ src/main/java/com/chc/challenge/twitter/webSocket/TwitterEstadoRecivido.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationEvent;
 import twitter4j.Status;
 @Getter
 public class TwitterEstadoRecivido extends ApplicationEvent {
-
+//recuperamos el objeto
     private final Status status;
     public TwitterEstadoRecivido(Object source,Status status) {
         super(source);
