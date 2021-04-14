f5a88398929b021aee31514a96c5941ab8878f82
carloshdzcasares <chcandristudio@gmail.com>
2021-04-14 19:55:39 +0200

    Configuración de credenciales por código y variables de entorno

--- src/main/java/com/chc/challenge/twitter/configuration/SpringConfig.java
+++ src/main/java/com/chc/challenge/twitter/configuration/SpringConfig.java
@@ -1,28 +1,9 @@
 package com.chc.challenge.twitter.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 
 @Configuration
 public class SpringConfig {
-    @Bean
-    TwitterStreamFactory twitterStreamFactory() {
-        return new TwitterStreamFactory();
-    }
-
-    @Bean
-    TwitterStream twitterStream(TwitterStreamFactory twitterStreamFactory) {
-        return twitterStreamFactory.getInstance();
-    }
-
-
 }
--- src/main/java/com/chc/challenge/twitter/configuration/TwitterConfig.java
+++ src/main/java/com/chc/challenge/twitter/configuration/TwitterConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 
 import javax.annotation.PostConstruct;
@@ -17,10 +18,22 @@ public class TwitterConfig {
 
     @Autowired
     TwitterListener twitterListener;
+    TwitterStream twitterStream;
 
     @PostConstruct
+    public void autentication(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(System.getenv("Dtwitter4j.oauth.consumerKey"))
+                .setOAuthConsumerSecret(System.getenv("Dtwitter4j.oauth.consumerSecret"))
+                .setOAuthAccessToken(System.getenv("Dtwitter4j.oauth.accessToken"))
+                .setOAuthAccessTokenSecret(System.getenv("Dtwitter4j.oauth.accessTokenSecret"));
+        TwitterStreamFactory tsf =new TwitterStreamFactory(cb.build());
+        twitterStream = tsf.getInstance();
+        configTwitterStream();
+    }
+
     public void configTwitterStream(){
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
         FilterQuery tweetFilterQuery = new FilterQuery();
         tweetFilterQuery.track(new String[]{"#","coronavirus","covid"});
         tweetFilterQuery.follow(new long[]{1500L});
